bb2dee4e2a3c55114cc62db002c68d343a0a8138
maciadalmau <47743853+maciadalmau@users.noreply.github.com>
2019-12-13 21:24:56 +0100

    Reversing

--- Physics3D_class5 - solution/ModulePlayer.cpp
+++ Physics3D_class5 - solution/ModulePlayer.cpp
@@ -137,7 +137,8 @@ update_status ModulePlayer::Update(float dt)
 
 	if(App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
 	{
-		brake = BRAKE_POWER;
+		if (vehicle->GetKmh() > 0) brake = BRAKE_POWER;
+		else acceleration = -500;
 	}
 
 	vehicle->ApplyEngineForce(acceleration);
